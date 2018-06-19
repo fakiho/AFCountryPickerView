12d480a905b4f50c0bebec4d11c20e8fd2fdb3b8
Ali Fakih <afdetails@gmail.com>
2018-06-19 10:49:52 +0300

    Add files via upload

---
+++ Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  CountryPickerView
+//
+//  Created by Ali Fakih on June/19/2018.
+//  Copyright © 2018 Ali Fakih. All rights reserved.
+//
+
+import UIKit
+
+extension UIWindow {
+    var topViewController: UIViewController? {
+        guard var top = rootViewController else {
+            return nil
+        }
+        while let next = top.presentedViewController {
+            top = next
+        }
+        return top
+    }
+}
+
+extension UINavigationController {
+    func popViewController(animated: Bool, completion: @escaping () -> Void) {
+        popViewController(animated: animated)
+        
+        if animated, let coordinator = transitionCoordinator {
+            coordinator.animate(alongsideTransition: nil) { _ in
+                completion()
+            }
+        } else {
+            completion()
+        }
+    }
+}
